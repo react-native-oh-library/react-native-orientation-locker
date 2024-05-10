d92954a59941eaa7c8868aa943f9d1e4bc11aeb9
maoqian <13477525645@139.com>
2024-05-10 17:09:04 +0800

    1.提交harmony文件；

---
+++ harmony/orientation_locker/src/main/ets/RNOrientationLockerTurboModule.ts
@@ -0,0 +1,133 @@
+import { TurboModule } from '@rnoh/react-native-openharmony/ts'
+import { TM } from '@rnoh/react-native-openharmony/generated/ts'
+import window from '@ohos.window'
+import display from '@ohos.display'
+import { BusinessError } from '@kit.BasicServicesKit'
+
+export class RNOrientationLockerTurboModule extends TurboModule implements TM.OreitationLockerNativeModule.Spec {
+  constructor(ctx) {
+    super(ctx)
+    display.on('change', () => {
+      let displayValue = display.getDefaultDisplaySync();
+      let displayValueString = this.getOrientationString(displayValue.orientation);
+      ctx.rnInstance.emitDeviceEvent('orientationDidChange', { orientation: displayValueString })
+      ctx.rnInstance.emitDeviceEvent('deviceOrientationDidChange', { deviceOrientation: displayValueString })
+    })
+  }
+
+  private windowClass: window.Window | undefined = undefined
+
+  private async setWindowClass(): Promise<void> {
+    let context = this.ctx.uiAbilityContext;
+    let promise = await window.getLastWindow(context);
+    this.windowClass = promise;
+    return;
+  }
+
+  private getOrientationString(orientation: number): string {
+    if (orientation === 0) {
+      return 'PORTRAIT';
+    } else if (orientation === 1) {
+      return 'LANDSCAPE_LEFT';
+    } else if (orientation === 2) {
+      return 'PORTRAIT-UPSIDEDOWN';
+    } else if (orientation === 3) {
+      return 'LANDSCAPE_RIGHT';
+    } else {
+      return 'UNKNOWN';
+    }
+  }
+
+  private  sendLockEvent(orientation: number) {
+    let orientationString = this.getOrientationString(orientation)
+    this.ctx.rnInstance.emitDeviceEvent('lockDidChange', { orientation: orientationString })
+  }
+
+  private lockToOrientation(orientation) {
+    this.setWindowClass().then(() => {
+      if (this.windowClass) {
+        // let orientation: window.Orientation = window.Orientation.AUTO_ROTATION_PORTRAIT;
+        this.windowClass.setPreferredOrientation(orientation, (err: BusinessError) => {
+          if (err.code) {
+            return;
+          }
+          this.sendLockEvent(orientation)
+        });
+      }
+    })
+  }
+
+  lockToPortrait(): void {
+    let orientation: window.Orientation = window.Orientation.PORTRAIT;
+    this.lockToOrientation(orientation)
+  }
+
+  lockToLandscape(): void {
+    let orientation: window.Orientation = window.Orientation.AUTO_ROTATION_LANDSCAPE_RESTRICTED;
+    this.lockToOrientation(orientation)
+  }
+
+  lockToLandscapeLeft(): void {
+    let orientation: window.Orientation = window.Orientation.LANDSCAPE;
+    this.lockToOrientation(orientation)
+  }
+
+  lockToAllOrientationsButUpsideDown(): void {
+    let orientation: window.Orientation = window.Orientation.AUTO_ROTATION_PORTRAIT;
+    this.lockToOrientation(orientation)
+  }
+
+  lockToLandscapeRight(): void {
+    let orientation: window.Orientation = window.Orientation.LANDSCAPE_INVERTED;
+    this.lockToOrientation(orientation)
+  }
+
+  lockToPortraitUpsideDown(): void {
+    let orientation: window.Orientation = window.Orientation.PORTRAIT_INVERTED;
+    this.lockToOrientation(orientation)
+  }
+
+  unlockAllOrientations(): void {
+    let orientation: window.Orientation = window.Orientation.AUTO_ROTATION;
+    this.lockToOrientation(orientation)
+  }
+
+  getOrientation(callback: (orientation: string) => void): void {
+    let displayClass: display.Display | null = null;
+    let err: string | null = null;
+    try {
+      displayClass = display.getDefaultDisplaySync();
+      let AppOrientation = this.getOrientationString(displayClass.orientation);
+      callback(AppOrientation);
+    } catch (e) {
+      callback(null);
+    }
+  }
+
+  getInitialOrientation(): string {
+    let displayClass: display.Display | null = null;
+    try {
+      displayClass = display.getDefaultDisplaySync();
+      let AppOrientation = this.getOrientationString(displayClass.orientation);
+      return AppOrientation
+    } catch (e) {
+      return 'UNKNOWN'
+    }
+  }
+
+  getDeviceOrientation(callback: (orientation: string) => void): void {
+    let displayClass: display.Display | null = null;
+    let err: string | null = null;
+    try {
+      displayClass = display.getDefaultDisplaySync();
+      let AppOrientation = this.getOrientationString(displayClass.orientation);
+      callback(AppOrientation);
+    } catch (e) {
+      callback(null);
+    }
+  }
+
+  getAutoRotateState(callback: (state: boolean) => void): void {
+   callback(true)
+  }
+}
---
+++ harmony/orientation_locker/ts.ts
@@ -0,0 +1,2 @@
+export * from './src/main/ets/RNOrientationLockerPackage'
+export * from './src/main/ets/RNOrientationLockerTurboModule'
