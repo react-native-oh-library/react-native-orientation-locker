02e99adc2754df7dc6fdf8b862bbec384daca7b2
chenwenhui133 <40195891+chenwenhui133@users.noreply.github.com>
2025-08-29 16:56:57 +0800

    Revert "fix:修改设置unlockAllOrientations时，系统锁定失效的问题"

--- harmony/orientation_locker/src/main/ets/RNOrientationLockerTurboModule.ts
+++ harmony/orientation_locker/src/main/ets/RNOrientationLockerTurboModule.ts
@@ -137,7 +137,7 @@ export class RNOrientationLockerTurboModule extends TurboModule implements TM.Or
   }
 
   unlockAllOrientations(): void {
-    let orientation: window.Orientation = window.Orientation.AUTO_ROTATION_RESTRICTED;
+    let orientation: window.Orientation = window.Orientation.AUTO_ROTATION;
     this.lockToOrientation(orientation)
   }
 
